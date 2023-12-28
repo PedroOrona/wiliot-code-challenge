c9876567da14c9f35fa537ae39972850c73e1ef6
PedroOrona <pedroaugusto.eng@gmail.com>
2023-12-28 15:15:35 +0000

    Change ECR name

--- infra/main.tf
+++ infra/main.tf
@@ -111,7 +111,7 @@ resource "aws_eks_addon" "ebs-csi" {
 module "ecr" {
   source = "terraform-aws-modules/ecr/aws"
 
-  repository_name = "private-example"
+  repository_name = "wiliot-location-app"
 
   # repository_read_write_access_arns = ["arn:aws:iam::012345678901:role/terraform"]
   repository_lifecycle_policy = jsonencode({
